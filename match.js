b245833c4b31d4df552e978d9758c7291199a8e8
Ryan Clements <rclements@innovasium.com>
2016-08-13 08:16:46 -0400

    fix file load

--- match.js
+++ match.js
@@ -2,7 +2,7 @@ var products = null;
 var listings = null;
 
 function LoadProducts() {
-  var oFrame = document.getElementById("frmFile");
+  var oFrame = document.getElementById("prodFile");
   var rawProducts = oFrame.contentWindow.document.body.childNodes[0].innerHTML;	
   
   while (rawProducts.indexOf("\n") >= 0) {
@@ -14,7 +14,7 @@ function LoadProducts() {
 }
 
 function LoadListings() {
-  var oFrame = document.getElementById("frmFile");
+  var oFrame = document.getElementById("listFile");
   var rawListings = oFrame.contentWindow.document.body.childNodes[0].innerHTML;	
   
   while (rawListings.indexOf("\n") >= 0) {
