85e36e3396b735f8ac3f378506111009e62591fe
Ryan Clements <rclements@innovasium.com>
2016-08-13 07:44:03 -0400

    load products

--- match.js
+++ match.js
@@ -1,6 +1,13 @@
+var products = null;
 
 function LoadProducts() {
   var oFrame = document.getElementById("frmFile");
   var strRawContents = oFrame.contentWindow.document.body.childNodes[0].innerHTML;	
-  console.log("ok", strRawContents);	
+  
+  while (strRawContents.indexOf("\r") >= 0) {
+    strRawContents = strRawContents.replace("\r", "");
+  }
+  products = strRawContents.split("\n");  	
+
+  console.log("ok", products);
 }
